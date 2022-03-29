cd4e5dbbfe7db06f19e3fcc3413b20ffd4bb77d0
Mauro Lepore <maurolepore@gmail.com>
2022-03-29 09:31:48 -0300

    Experience pain

--- 03_all-is-fail/debugme.R
+++ 03_all-is-fail/debugme.R
@@ -17,22 +17,4 @@ result <- dataset %>%
   compute(show_progress = TRUE) %>%
   use_cache("result")  # a) Reuse computations; b) Avoid hard-coding paths
 
-result
-
-
-
-
-# FIXME
-library(tidyverse)
-devtools::load_all()
-
-uniq <- europages_bs_summary %>%
-  group_by(url_info) %>%
-  filter(row_number() == 1L) %>%
-  ungroup() %>%
-  mutate(path_info = path_info(url_info)) %>%
-  use_pin("uniq")
-
-uniq <- tail(uniq, 1000)
-dir_create(unique(path_dir(uniq$path_info)))
-write_rendered_html(url = uniq$url_info, path = uniq$path_info, overwrite = FALSE)
+write_rendered_html(url = result$url, path = result$path, overwrite = FALSE)
--- R/functions.R
+++ R/functions.R
@@ -2,6 +2,8 @@ library(pins)
 library(glue)
 library(here)
 library(fs)
+# devtools::install_github("rstudio/chromote")
+library(chromote)
 
 use_cache <- function(x, name, board = board_local(), overwrite = FALSE) {
   if (!pin_exists(board, name) || overwrite) pin_write(board, x, name)
@@ -36,4 +38,35 @@ extract_break <- function(url) {
 
 make_html_path <- function(url) {
   path_rel(here("html", glue("{extract_break(url)}.qs")), here())
+}
+
+# Write the dom as html -- generated from a headless browser
+write_rendered_html <- function(url, path, overwrite = FALSE) {
+  session <- start_chrome_session()
+
+  for (i in seq_along(url)) {
+    if (!overwrite && file_exists(path[[i]])) {
+      next()
+    }
+    html <- render_html(url[[i]], path[[i]], session)
+    qsave(html, path[[i]])
+  }
+
+  end_chrome_session()
+
+  invisible(url)
+}
+
+start_chrome_session <- function() {
+  chromote::ChromoteSession$new()
+}
+
+end_chrome_session <- function(session) {
+  session$close()
+}
+
+render_html <- function(url, path, session) {
+  session$Page$navigate(url)
+  session$Page$loadEventFired()
+  session$Runtime$evaluate("document.documentElement.outerHTML")$result$value
 }
